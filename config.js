3e072d00c1d882a19b7f41185073b46e32a54458
Qtram98 <88839665+Qtram98@users.noreply.github.com>
2021-08-15 12:53:01 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,16 @@
 const CONFIG = {
-    introTitle: 'Dear Bae, Hoàng Nhật Ân!',
-    introDesc: ` Hôm nay là ngày 20/10/2021- tròn 2 năm bên nhau nhé!`,
+    introTitle: 'Dearf!',
+    introDesc: ` Hôm nay là ngày 20/10/2021- tròg bên nhau nhé!`,
     btnIntro: 'Love diu',
     title: 'Cảm ơn đã đến bên em',
     desc: 'Anh có yêu em không??? ',
     btnYes: 'KÓ <33',
     btnNo: 'Yêu nhiều lắmmmmm',
     question:'Gửi gắm đôi lời cho em nàooo <3',
-    btnReply: 'Gửi cho Trâm <3',
+    btnReply: 'Gửi ch<3',
     reply: 'Em là dữ dằn nhứt <33333333',
     mess: 'Em biết mà 🥰. Cảm ơn anh đã chịu đựng và yêu e suốt thời gian qua, hi vọng cùng nhau bước chặng đường dài 😘😘',
     messDesc: 'Tối nay gặp nhau nhé ^^.',
     btnAccept: 'Dạ,Miễn là có e bên cạnh hihi <3',
-    messLink: 'http://m.me/quynhtram.huynhthi' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'http://m.me/quynhti' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
