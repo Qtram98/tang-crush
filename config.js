c1f312ffbd99975829a649fdef5186cead58d65b
Qtram98 <88839665+Qtram98@users.noreply.github.com>
2021-08-15 11:48:08 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    introTitle: 'Dear Bae, Hoàng Nhật Ân!',
+    introDesc: ` Hôm nay là ngày 20/10/2021- tròn 2 năm bên nhau nhé!`,
+    btnIntro: 'Love diu',
+    title: 'Cảm ơn đã đến bên em',
+    desc: 'Anh có yêu em không??? ',
+    btnYes: 'KÓ <33',
+    btnNo: 'Yêu nhiều lắmmmmm',
+    question:'Gửi gắm đôi lời cho em nàooo <3',
+    btnReply: 'Gửi cho Trâm <3',
+    reply: 'Em là dữ dằn nhứt <33333333',
+    mess: 'Em biết mà 🥰. Cảm ơn anh đã chịu đựng và yêu e suốt thời gian qua, hi vọng cùng nhau bước chặng đường dài 😘😘',
+    messDesc: 'Tối nay gặp nhau nhé ^^.',
+    btnAccept: 'Dạ,Miễn là có e bên cạnh hihi <3',
+    messLink: 'http://m.me/quynhtram.huynhthi' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
